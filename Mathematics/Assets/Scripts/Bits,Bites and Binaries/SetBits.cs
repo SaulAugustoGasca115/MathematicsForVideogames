352cb1a2b6f47af4f4f441ebd55c5e00b82fc97e
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-03-01 13:30:09 -0600

    Project Created and Bit script to convert decimal to binary number in code added

---
+++ Mathematics/Assets/Scripts/Bits,Bites and Binaries/SetBits.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SetBits : MonoBehaviour
+{
+    [Header("Bits")]
+    public int bitSequence =  8 + 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("<color=green>BIT: " + Convert.ToString(bitSequence,2) + "</color>");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
