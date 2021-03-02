d1e7beebe4662e3ea80653951f47421087b2f4ea
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-03-01 21:28:52 -0600

    Power Objects Created (Magic,Intelligence,Charisma,Invisible and Fly) ,Bit Flags, Bitwise operators and Turning Bit Flags On

---
+++ Mathematics/Assets/Scripts/Bits,Bites and Binaries/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [Header("Camera Follow")]
+    public Transform player;
+    public float distance = 10;
+    public float height = 5;
+    public Vector3 lookOffset = new Vector3(0, 1, 0);
+    public float cameraSpeed = 100f;
+    public float rotationSpeed = 100f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(player)
+        {
+            Vector3 lookPosition = player.position + lookOffset;
+            Vector3 relativePosition = lookPosition - transform.position;
+
+            Quaternion rotation = Quaternion.LookRotation(relativePosition);
+
+            transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.deltaTime * rotationSpeed * 0.1f);
+
+            Vector3 targetPosition = player.position + player.up * height - player.forward * distance;
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * cameraSpeed * 0.1f);
+
+        }
+    }
+}
---
+++ Mathematics/Assets/Scripts/Bits,Bites and Binaries/OwnAttributeManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OwnAttributeManager : MonoBehaviour
+{
+
+    [Header("UI Elements")]
+    public Text attributeDisplay;
+    
+
+    [Header("Attributes")]
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+    public int attributes = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+        attributeDisplay.transform.position = screenPoint + new Vector3(0, -18f, 0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+        attributeDisplay.color = Color.green;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "MAGIC")
+        {
+            attributes |= MAGIC;
+        }
+        else if(other.gameObject.tag == "INTELLIGENCE")
+        {
+            attributes |= INTELLIGENCE;
+        }
+        else if(other.gameObject.tag == "CHARISMA")
+        {
+            attributes |= CHARISMA;
+            //Destroy(other.gameObject);
+        }
+        else if(other.gameObject.tag == "FLY")
+        {
+            attributes |= FLY;
+        }
+        else if(other.gameObject.tag == "INVISIBLE")
+        {
+            attributes |= INVISIBLE;
+        }else if(other.gameObject.tag == "ANTIMAGIC")
+        {
+            attributes &= ~MAGIC;
+        }
+        else if (other.gameObject.tag == "REMOVE")
+        {
+            attributes &= ~(INTELLIGENCE | CHARISMA | INVISIBLE | MAGIC );
+        }
+        else if (other.gameObject.tag == "ADD")
+        {
+            attributes |= (INVISIBLE);
+        }
+        else if (other.gameObject.tag == "RESET")
+        {
+            attributes = 0;
+        }
+    }
+}
